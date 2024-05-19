debb1cb31e573adab3175c1fe77f27035f0b120c
Johnson Giang <johnson.giang21@gmail.com>
2024-05-18 20:09:22 -0600

    got leagueleader page to work

--- components/Leagueleader.js
+++ components/Leagueleader.js
@@ -54,12 +54,30 @@ const LeadersList = () => {
                     className="form-select block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
                     onChange={(e) => setCategory(e.target.value)}
                 >
-                    <option value="MP">MPG</option>
-                    <option value="PTS">PPG</option>
-                    <option value="TRB">RPG</option>
-                    <option value="AST">APG</option>
-                    <option value="STL">SPG</option>
-                    <option value="BLK">BPG</option>
+                    <option value="MP">Minutes Per Game (MPG)</option>
+                    <option value="PTS">Points Per Game (PPG)</option>
+                    <option value="TRB">Rebounds Per Game (RPG)</option>
+                    <option value="AST">Assists Per Game (APG)</option>
+                    <option value="STL">Steals Per Game (SPG)</option>
+                    <option value="BLK">Blocks Per Game (BPG)</option>
+                    <option value="FG">Field Goals Made (FGM)</option>
+                    <option value="FGA">Field Goals Attempted (FGA)</option>
+                    <option value="FGPercent">Field Goal Percentage (FG%)</option>
+                    <option value="threeP">Three-Point Field Goals Made (3PM)</option>
+                    <option value="threePA">Three-Point Field Goals Attempted (3PA)</option>
+                    <option value="threePPercent">Three-Point Field Goal Percentage (3P%)</option>
+                    <option value="twoP">Two-Point Field Goals Made (2PM)</option>
+                    <option value="twoPA">Two-Point Field Goals Attempted (2PA)</option>
+                    <option value="twoPPercent">Two-Point Field Goal Percentage (2P%)</option>
+                    <option value="eFGPercent">Effective Field Goal Percentage (eFG%)</option>
+                    <option value="FT">Free Throws Made (FTM)</option>
+                    <option value="FTA">Free Throws Attempted (FTA)</option>
+                    <option value="FTPercent">Free Throw Percentage (FT%)</option>
+                    <option value="ORB">Offensive Rebounds (ORB)</option>
+                    <option value="DRB">Defensive Rebounds (DRB)</option>
+                    <option value="TOV">Turnovers (TOV)</option>
+                    <option value="PF">Personal Fouls (PF)</option>
+
                 </select>
             </div>
             {renderTopPlayers(category)}
