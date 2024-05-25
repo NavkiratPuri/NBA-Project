761aa5afd6bec65087aee82a6b292e4c71e36c5d
Navkirat Puri <140457894+NavkiratPuri@users.noreply.github.com>
2024-05-24 19:22:20 -0600

    hashing password implemented
    hashing works need to figure out how to unencrypt?

--- app/api/dbuser/route.js
+++ app/api/dbuser/route.js
@@ -2,20 +2,30 @@ import client from "@/app/libs/prismadb";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 
+
 // URL: http://localhost:3000/api/dbuser
 
 // handles POST requests of email and password to create a new dbuser, returns new dbuser if
 // successful, or an error message if not
 
+
+
 export const POST = async (req) => {
+
+    
+
     try {
         const body = await req.json();
         const { email, password } = body;
         const hashedPassword = await bcrypt.hash(password, 10);
+        console.log(email, hashedPassword); 
+        
+        
+        console.log(password);
         const newDbUser = await client.dbuser.create({
             data: {
                 email,
-                hashedPassword
+                password: hashedPassword
             },
         });
         return NextResponse.json(newDbUser);
