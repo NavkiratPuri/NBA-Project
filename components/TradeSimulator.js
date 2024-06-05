513ec6632323575aa373fa743f68a47793bd39f8
Johnson Giang <johnson.giang21@gmail.com>
2024-06-05 03:11:57 -0600

    Update TradeSimulator.js
    zains update to trade simulator

--- components/TradeSimulator.js
+++ components/TradeSimulator.js
@@ -10,17 +10,17 @@ const TradeSimulator = ({ player, valueColor }) => {
         <div className="mt-4 p-4 bg-gray-50 rounded-lg shadow-md">
             {player &&
                 (
-                <div className="space-y-2">
-                    <div>
-                        <p>Team: {player.Tm}</p>
-                        <p>Points Per Game: {player.PTS}</p>
-                        <p>Assists Per Game: {player.AST}</p>
-                        <p>Blocks Per Game: {player.BLK}</p>
-                        <p>Steals Per Game: {player.STL}</p>
-                        <p>Rebounds Per Game: {(player.TRB)}</p>
-                        <p className={`text-2xl font-bold ${valueColor}`}>Value: {calculatePlayerValue(player)}</p>
+                    <div className="space-y-2">
+                        <div>
+                            <p>Team: {player.Tm}</p>
+                            <p>Points Per Game: {player.PTS}</p>
+                            <p>Assists Per Game: {player.AST}</p>
+                            <p>Blocks Per Game: {player.BLK}</p>
+                            <p>Steals Per Game: {player.STL}</p>
+                            <p>Rebounds Per Game: {(player.TRB)}</p>
+                            <p className={`text-2xl font-bold ${valueColor}`}>Value: {calculatePlayerValue(player)}</p>
+                        </div>
                     </div>
-                </div>
                 )}
         </div>
     );
