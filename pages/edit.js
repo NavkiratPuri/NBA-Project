2c978d5006078b7f37b76c6d94d94ea8c4ea19ab
Johnson Giang <johnson.giang21@gmail.com>
2024-06-05 18:18:09 -0600

    removed fetching code for league leader and edit page

--- pages/edit.js
+++ pages/edit.js
@@ -7,27 +7,6 @@ import Footer from '../components/footer';
 
 // Modify Player Page
 const Add = () => {
-    const [Player, setPlayer] = useState([]);
-
-    // useEffect hook to fetch data from the server
-    // In your Add component or wherever PlayerList is being rendered
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch('/api/player', { cache: "no-cache" });
-                if (!res.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                const data = await res.json();
-                console.log('Data received:', data); // Verify the structure here
-                setPlayer(data); // Assuming setPlayer is the state setter for players
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
 
 
     return (
@@ -35,7 +14,7 @@ const Add = () => {
             <Header />
             <h1 className="text-3xl font-bold text-center text-gray-800 mt-4 mb-4">Modify Player</h1>
             <div className="flex-grow">
-                <PlayerList players={Player} />
+                <PlayerList />
                 <div className="py-8 ml-4">
                     <Addplayer />
                 </div>
--- pages/leader.js
+++ pages/leader.js
@@ -7,31 +7,12 @@ import Footer from '../components/footer';
 // Leaders Page for best performing players
 
 const Leaders = () => {
-    const [players, setPlayers] = useState([]);
-
-    // useEffect hook to fetch data from the server
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch('/api/player', { cache: "no-cache" });
-                if (!res.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                const data = await res.json();
-                setPlayers(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
 
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
             <main className="flex-grow p-8 pb-20">
-                <LeadersList players={players} />
+                <LeadersList />
             </main>
             <Footer />
         </div>
