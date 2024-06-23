a0f5d2c270135de68127a33227ac9248ed3a42e1
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-06-23 00:20:09 -0600

    Update prismadb.js

--- app/libs/prismadb.js
+++ app/libs/prismadb.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.PrismaClient || new PrismaClient();
+const client = globalThis.prisma || new PrismaClient();
 
 if(process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
