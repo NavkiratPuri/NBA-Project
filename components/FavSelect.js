0ebe52ba30bcefaaad3133f0491a02e4f54c050f
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-07-03 13:37:59 -0600

    Create FavSelect.js

---
+++ components/FavSelect.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+import axios from "axios";
+
+
+const FavSelect = () => {
+    
+
+    // Handles form submission to POST data to the server
+    const handleSubmit = async (event) => {
+       
+    };
+
+    return (
+        <div>
+            
+            
+        </div>
+    );
+};
+
+export default FavSelect;
