93dd76c67e0d92607f7588fc8d6b3ab8f597cfac
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-07-10 15:04:56 -0600

    Update FavTeam.js
    untested

--- components/FavTeam.js
+++ components/FavTeam.js
@@ -3,17 +3,37 @@ import React, { useState, useEffect } from 'react';
 import TeamSelector from '@/components/TeamSelector';
 import teamsData from '@/utils/teamsData';
 import axios from 'axios';
+import FavTeamDisplay from './FavTeamDisplay';
+import fetchTeam from '@/utils/fetchTeam';
 
 
-
-const FavTeam = () => {
+const FavTeam = ({teamId}) => {
     
     const [teams, setTeams] = useState([]);
     const [selectedTeam, setSelectedTeam] = useState([null]);
     const [favTeam, setFavTeam] = useState(null);
-    const [favTeamId, setTeamId] = useState(null);
+    const [favTeamId, setTeamId] = useState(teamId);
     
-    
+    useEffect(() => {
+
+
+        const getFavTeamData = async () => {
+            try{
+                const data = await fetchTeam(teamId);
+                console.log('data:', data);
+                setFavTeam(data);
+            } catch (err){
+                setError(err.message);
+            }
+        };
+
+        if (teamId) {
+            getFavTeamData();
+            console.log('teamId:', teamId);
+        }
+    }, [teamId]);
+
+
     useEffect(() => {
         const fetchTeams = async () => {
             try {
@@ -25,6 +45,27 @@ const FavTeam = () => {
         };
         fetchTeams();
     }, []);
+
+    useEffect(() => {
+        console.log('Updated favTeamId:', favTeamId);
+      }, [favTeamId]);
+
+    const handleSelectTeam = (team) => {
+        setFavTeam(team);
+        setTeamId(team.id);
+        updateFavTeam(team.id);
+        
+    };
+
+    const updateFavTeam = async (teamId) => {
+        try {
+            const response = await axios.put(`/api/user`, {favTeamId: teamId});
+            console.log('response:', response);
+        } catch (error) {
+            console.error('Error updating favTeam:', error);
+        }
+    }
+
 
 
 
