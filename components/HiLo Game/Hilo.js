911499751ef86e23d1f8ac31234afd4baaca4f7d
SoftOnigiri <89949768+SoftOnigiri@users.noreply.github.com>
2024-07-17 18:52:22 -0600

    update

--- components/HiLo Game/Hilo.js
+++ components/HiLo Game/Hilo.js
@@ -170,13 +170,7 @@ const HiLo = ({ onGameEnd }) => {
             <div className="relative flex-grow">
                 {gameStatus === "ongoing" ? (
                     <>
-                        <GameInfo
-                            displayCategory={displayCategory}
-                            lives={lives}
-                            points={points}
-                            comparisonResult={comparisonResult}
-                            hint={hint}
-                        />
+                        
 
                         {players.length > 0 && selectedCategory && (
                             <div className="flex">
@@ -196,6 +190,14 @@ const HiLo = ({ onGameEnd }) => {
                                 ))}
                             </div>
                         )}
+
+                        <GameInfo
+                            displayCategory={displayCategory}
+                            lives={lives}
+                            points={points}
+                            comparisonResult={comparisonResult}
+                            hint={hint}
+                        />
                     </>
                 ) : (
                     <GameOver
