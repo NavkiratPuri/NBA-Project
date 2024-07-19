61317d7db0679e0eefd576e4055e49ca0b77c4a2
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-07-19 14:33:02 -0600

    Create currentPlayer.js to get data on only current players

---
+++ utils/currentPlayers.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+// function to fetch data from mongodb
+const currentPlayers = async () => {
+    try{
+        const response = await axios.get('api/player');
+        if (response.data) {
+            return response.data;
+        }
+    }catch (error){
+        console.error('error fetching player data', error);
+    }
+};
+
+export default currentPlayers;
