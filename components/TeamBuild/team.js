6e1fcdfabdf400a0dc3691da3f8ab859b8a1cbf3
SoftOnigiri <89949768+SoftOnigiri@users.noreply.github.com>
2024-07-25 00:01:42 -0600

    UPDATE
    - added way to remove players from final list
    - removes team when chosen
    - changed layout - tailwind
    -

--- components/TeamBuild/team.js
+++ components/TeamBuild/team.js
@@ -35,7 +35,7 @@ const TeamBuilder = () => {
         'SG': null
     });
 
-    const [usedTeams, setUsedTeams] = useState([]);
+    const [availableTeams, setAvailableTeams] = useState([]);
     const [usedPositions, setUsedPositions] = useState([]);
 
     useEffect(() => {
@@ -53,6 +53,7 @@ const TeamBuilder = () => {
                 setPlayers(playersData);
                 const initialTeams = getRandomTeams(playersData);
                 setTeams(initialTeams);
+                setAvailableTeams(initialTeams); // Initialize available teams
                 const filteredPlayers = filterPlayersByTeams(playersData, initialTeams);
                 setCategorizedPlayers(categorizePlayersByPosition(filteredPlayers));
             } catch (error) {
@@ -66,6 +67,7 @@ const TeamBuilder = () => {
     const updateTeamsAndPlayers = (playersData) => {
         const newTeams = getRandomTeams(playersData);
         setTeams(newTeams);
+        setAvailableTeams(newTeams); // Update available teams
         const filteredPlayers = filterPlayersByTeams(playersData, newTeams);
         setCategorizedPlayers(categorizePlayersByPosition(filteredPlayers));
     };
@@ -76,8 +78,24 @@ const TeamBuilder = () => {
             [position]: player
         }));
 
-        setUsedTeams(prevState => [...prevState, player.Tm]);
+        // Remove selected team from available teams
+        setAvailableTeams(prevTeams => prevTeams.filter(team => team !== player.Tm));
         setUsedPositions(prevState => [...prevState, position]);
+    };
+
+    const handleDeselectPlayer = (position) => {
+        const player = selectedPlayers[position];
+        if (player) {
+            // Re-add the team to the available teams
+            setAvailableTeams(prevTeams => [...prevTeams, player.Tm]);
+            // Remove the player from the selected players
+            setSelectedPlayers(prevState => ({
+                ...prevState,
+                [position]: null
+            }));
+            // Remove position from used positions
+            setUsedPositions(prevState => prevState.filter(pos => pos !== position));
+        }
     };
 
     const handleReset = async () => {
@@ -98,7 +116,6 @@ const TeamBuilder = () => {
                 'PG': null,
                 'SG': null
             });
-            setUsedTeams([]);
             setUsedPositions([]);
             updateTeamsAndPlayers(playersData);
         } catch (error) {
@@ -107,55 +124,66 @@ const TeamBuilder = () => {
     };
 
     const filteredPlayersByPosition = (position) => {
-        return categorizedPlayers[position]?.filter(player => !usedTeams.includes(player.Tm)) || [];
+        return categorizedPlayers[position]?.filter(player => availableTeams.includes(player.Tm)) || [];
     };
 
     return (
-        <div>
-            {Object.keys(categorizedPlayers).map(position => (
-                !usedPositions.includes(position) && (
-                    <PlayerSelector
-                        key={position}
-                        players={filteredPlayersByPosition(position)}
-                        onSelectPlayer={(player) => handleSelectPlayer(position, player)}
-                        label={position}
-                        teams={teams} // Pass teams here
-                    />
-                )
-            ))}
-
-            <div className="selected-players mt-8 p-4 border-t border-gray-300">
-                <h2 className="text-lg font-bold mb-4">Selected Players</h2>
-                {Object.entries(selectedPlayers).map(([position, player]) => (
-                    player && (
-                        <div key={position} className="mb-2 p-2 border border-gray-300 rounded">
-                            <h3 className="text-md font-semibold">{position}</h3>
-                            <p>{player.Player} - {player.Tm} - {player.Year}</p>
-                        </div>
-                    )
-                ))}
-            </div>
-
-            <div className="available-teams mt-8 p-4 border-t border-gray-300">
-                <h2 className="text-lg font-bold mb-4">Available Teams</h2>
-                <ul>
-                    {teams.map((team) => (
-                        !usedTeams.includes(team) && (
-                            <li key={team} className="mb-2 p-2 border border-gray-300 rounded">
-                                {team}
-                            </li>
+        <div className="container mx-auto p-4">
+            <div className="flex space-x-8">
+                <div className="w-1/2">
+                    {Object.keys(categorizedPlayers).map(position => (
+                        !usedPositions.includes(position) && (
+                            <PlayerSelector
+                                key={position}
+                                players={filteredPlayersByPosition(position)}
+                                onSelectPlayer={(player) => handleSelectPlayer(position, player)}
+                                label={position}
+                                teams={availableTeams} // Pass available teams here
+                            />
                         )
                     ))}
-                </ul>
-            </div>
+                </div>
+                <div className="w-1/2">
+                    <div className="selected-players mb-8 p-4 border-t border-gray-300">
+                        <h2 className="text-lg font-bold mb-4">Selected Players</h2>
+                        {Object.entries(selectedPlayers).map(([position, player]) => (
+                            player && (
+                                <div key={position} className="mb-2 p-2 border border-gray-300 rounded flex justify-between items-center">
+                                    <div>
+                                        <h3 className="text-md font-semibold">{position}</h3>
+                                        <p>{player.Player} - {player.Tm} - {player.Year}</p>
+                                    </div>
+                                    <button
+                                        onClick={() => handleDeselectPlayer(position)}
+                                        className="ml-4 p-2 bg-red-500 text-white rounded hover:bg-red-700"
+                                    >
+                                        Remove
+                                    </button>
+                                </div>
+                            )
+                        ))}
+                    </div>
 
-            <div className="reset-button mt-8 p-4">
-                <button
-                    onClick={handleReset}
-                    className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-                >
-                    Reset
-                </button>
+                    <div className="available-teams mb-8 p-4 border-t border-gray-300">
+                        <h2 className="text-lg font-bold mb-4">Available Teams</h2>
+                        <ul>
+                            {availableTeams.map((team) => (
+                                <li key={team} className="mb-2 p-2 border border-gray-300 rounded">
+                                    {team}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="reset-button p-4">
+                        <button
+                            onClick={handleReset}
+                            className="p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                        >
+                            Reset
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
