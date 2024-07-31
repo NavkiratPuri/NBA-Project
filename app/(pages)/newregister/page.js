e8f9e39cc5b65a8495a4e5f56debc71e4e00facb
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-07-31 15:33:31 -0600

    regex for ensuring user email is proper

--- app/(pages)/newregister/page.js
+++ app/(pages)/newregister/page.js
@@ -6,126 +6,133 @@ import { useRouter } from "next/navigation"
 import Login from "@/app/page"
 import LoginHeader from "@/components/LoginHeader"
 
-
-
-
-
 export default function Register() {
 
     const [data, setData] = useState({name: '', email: '', password: ''})
-    
-    
+    const [emailError, setEmailError] = useState('')
+
     const router = useRouter()
 
+    const validateEmail = (email) => {
+        const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/
+        return re.test(String(email).toLowerCase())
+    }
 
     const registerUser = async (e) => {
         e.preventDefault()
+        
+        if (!validateEmail(data.email)) {
+            setEmailError('Please enter a valid email address.')
+            return
+        }
+
         axios.post('/api/newregister', data)
-        .then(()=>{alert('User has been registered')
-          console.log('user has been registered', data)
-          router.push('/')
+        .then(()=> {
+            alert('User has been registered')
+            console.log('User has been registered', data)
+            router.push('/')
         })
-        .catch(()=>alert('Something went wrong') )
+        .catch(()=>alert('Something went wrong'))
     }
-
-
 
     return (
         <>
-          <LoginHeader />
-          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              
-              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Register for an account
-              </h2>
-            </div>
-    
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" onSubmit={registerUser}>
-                <div>
-                  <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
-                    Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="name"
-                      name="name"
-                      type="text"
-                      required
-                      value={data.name}
-                      onChange={e => setData({...data, name: e.target.value})}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
+            <LoginHeader />
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                        Register for an account
+                    </h2>
                 </div>
-  
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      value={data.email}
-                      onChange={e => setData({...data, email: e.target.value})}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                      Password
-                    </label>
-                    {/* <div className="text-sm">
-                      <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                        Forgot password?
-                      </a>
-                    </div> */}
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      value={data.password}
-                      onChange={e => setData({...data, password: e.target.value})}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Register
-                  </button>
-                </div>
-              </form>
 
-              <div className="mt-12 space-y-4 text-center">
-                <p className="text-sm text-gray-600">Already have an account?</p>
-                <button
-                  onClick={() => router.push('/')}
-                  className="w-full bg-indigo-900 text-white text-sm font-semibold py-2 rounded-md shadow-md hover:bg-gray-800 transition duration-200"
-                >
-                  Go to Login Page
-                </button>
-              </div>
-    
-              
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form className="space-y-6" onSubmit={registerUser}>
+                        <div>
+                            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                                Name
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    required
+                                    value={data.name}
+                                    onChange={e => setData({...data, name: e.target.value})}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                Email address
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="email"
+                                    name="email"
+                                    type="email"
+                                    autoComplete="email"
+                                    required
+                                    value={data.email}
+                                    onChange={e => {
+                                        setData({...data, email: e.target.value})
+                                        setEmailError('')
+                                    }}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                                {emailError && <p className="mt-2 text-sm text-red-600">{emailError}</p>}
+                            </div>
+                        </div>
+
+                        <div>
+                            <div className="flex items-center justify-between">
+                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Password
+                                </label>
+                                {/* <div className="text-sm">
+                                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                                        Forgot password?
+                                    </a>
+                                </div> */}
+                            </div>
+                            <div className="mt-2">
+                                <input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                    required
+                                    value={data.password}
+                                    onChange={e => setData({...data, password: e.target.value})}
+                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                />
+                            </div>
+                        </div>
+
+                        <div>
+                            <button
+                                type="submit"
+                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Register
+                            </button>
+                        </div>
+                    </form>
+
+                    <div className="mt-12 space-y-4 text-center">
+                        <p className="text-sm text-gray-600">Already have an account?</p>
+                        <button
+                            onClick={() => router.push('/')}
+                            className="w-full bg-indigo-900 text-white text-sm font-semibold py-2 rounded-md shadow-md hover:bg-gray-800 transition duration-200"
+                        >
+                            Go to Login Page
+                        </button>
+                    </div>
+                </div>
             </div>
-          </div>
         </>
-      )
-    }
+    )
+}
+
