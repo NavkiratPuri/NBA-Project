66c9d211418bb87b1a66d3ac75069fcdae8b7fda
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-07-31 13:58:46 -0600

    login header made and added to login page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react"
 import {signIn, useSession } from 'next-auth/react'
 import { useRouter } from "next/navigation"
+import LoginHeader from "@/components/LoginHeader"
 
 
 
@@ -42,6 +43,7 @@ export default function Login() {
 
     return (
         <>
+          <LoginHeader />
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               
---
+++ components/LoginHeader.js
@@ -0,0 +1,19 @@
+
+// Header component to display in login and signup pages
+const LoginHeader = () => {
+  
+
+  return (
+    <header className="bg-gray-800 text-white shadow-lg py-3 p-4 border-b border-white">
+      <div className="container mx-auto flex justify-center items-center">
+        <div className="flex items-center space-x-4">
+          <img src="/bpa.webp" alt="NBA Logo" className="h-20 w-auto" />
+          <h1 className="text-3xl font-bold">NBAlytics</h1>
+        </div>
+        
+      </div>
+    </header>
+  );
+};
+
+export default LoginHeader;
