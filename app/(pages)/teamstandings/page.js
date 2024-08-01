952aeba4b68358c213d2d8255abcc46c2ede5b62
Johnson Giang <johnson.giang21@gmail.com>
2024-08-01 04:39:38 -0600

    added footer to my pages

--- app/(pages)/teamstandings/page.js
+++ app/(pages)/teamstandings/page.js
@@ -3,12 +3,13 @@ import React from 'react';
 import TeamStandings from "@/components/Teamstandings";
 import Header from '@/components/header';
 //import '../app/globals.css';
-
+import Footer from '@/components/footer';
 const Teams = () => {
     return (
         <div>
             <Header />
             <TeamStandings />
+            <Footer />
         </div>
     );
 };
--- app/(pages)/trivia/page.js
+++ app/(pages)/trivia/page.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Trivia from "@/components/Trivia";
 import Header from "@/components/header";
 import AddQuestion from "@/components/Addquestion";
+import Footer from "@/components/footer";
 
 const TriviaPage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -10,16 +11,19 @@ const TriviaPage = () => {
   return (
     <div>
       <Header />
-      <button
-        className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
-        onClick={() => setShowModal(true)}
-      >
-        Add a new trivia question
-      </button>
       <Trivia />
       {showModal && (
         <AddQuestion showModal={showModal} setShowModal={setShowModal} />
       )}
+      <div className="px-4 pb-4"> {/* Added padding around the button */}
+        <button
+          className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
+          onClick={() => setShowModal(true)}
+        >
+          Add a new trivia question
+        </button>
+      </div>
+      <Footer />
     </div>
   );
 };
