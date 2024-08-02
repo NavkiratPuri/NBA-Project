d6b549efe8519793b543ae24d37ca98093e2252b
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-01 21:39:05 -0600

    standings protected

--- app/(pages)/standings/page.js
+++ app/(pages)/standings/page.js
@@ -4,7 +4,34 @@ import Header from '@/components/header';
 //import '../app/globals.css';
 import Footer from '@/components/footer';
 import StandingsDisplay from '@/components/StandingsDisplay';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+
+
+
 const Standings = () => {
+
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+          router.push("/");
+        } else if (status === "authenticated") {
+          
+            setLoading(false);
+          
+        }
+    }, [status, router]);
+
+    if (status === "loading" || loading) {
+        return <p>Loading...</p>;
+    }
+
+
     return (
         <div>
             <Header />
