b8c486de77b92d8af8cfd08a770fca0ccccfec08
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-01 20:01:55 -0600

    add button conditionly renders

--- app/(pages)/trivia/page.js
+++ app/(pages)/trivia/page.js
@@ -4,9 +4,11 @@ import Trivia from "@/components/Trivia";
 import Header from "@/components/header";
 import AddQuestion from "@/components/Addquestion";
 import Footer from "@/components/footer";
+import { useSession } from "next-auth/react";
 
 const TriviaPage = () => {
   const [showModal, setShowModal] = useState(false);
+  const { data: session, status } = useSession();
 
   return (
     <div>
@@ -16,12 +18,16 @@ const TriviaPage = () => {
         <AddQuestion showModal={showModal} setShowModal={setShowModal} />
       )}
       <div className="px-4 pb-4"> {/* Added padding around the button */}
-        <button
-          className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
-          onClick={() => setShowModal(true)}
-        >
-          Add a new trivia question
-        </button>
+      { session?.user?.isAdmin && (
+          <>
+            <button
+              className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
+              onClick={() => setShowModal(true)}
+            >
+              Add a new trivia question
+            </button>
+          </> 
+      )}
       </div>
       <Footer />
     </div>
