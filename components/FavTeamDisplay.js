9a55b6d620e420a65515645666e97c32a6905efc
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-02 03:06:42 -0600

    team logo images showing

--- components/FavTeamDisplay.js
+++ components/FavTeamDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const FavTeamDisplay = ({ team }) => {
   return (
@@ -6,6 +7,15 @@ const FavTeamDisplay = ({ team }) => {
       {team ? (
         <div className="space-y-2 text-center">
           <h2 className="text-2xl font-semibold text-indigo-900">Favorite Team</h2>
+          <div className="flex flex-col items-center space-y-2">
+            <Image
+                  src={team.logo}
+                  alt={`Image of ${team.team} logo`}
+                  width={64}
+                  height={64}
+                  className="w-16 h-16 rounded-md border-2 border-gray-600"
+                />
+          </div>
           <div className="flex flex-col items-center space-y-2">
             <h1 className="text-lg font-medium text-gray-700">{team.team}</h1>
             <p className="text-lg font-medium text-gray-700">Wins: {team.wins}</p>
