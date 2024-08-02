1701843be218817eed7dbce03b14f8c47c2d6aea
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-01 21:42:17 -0600

    teamstandings page protected

--- app/(pages)/teamstandings/page.js
+++ app/(pages)/teamstandings/page.js
@@ -4,7 +4,30 @@ import TeamStandings from "@/components/Teamstandings";
 import Header from '@/components/header';
 //import '../app/globals.css';
 import Footer from '@/components/footer';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+
 const Teams = () => {
+
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (status === "unauthenticated" ) {
+          router.push("/");
+        } 
+        if (session && !session.user.isAdmin) {
+          router.push("/");
+        }
+    }, [status, session, router]);
+
+    if (status === "loading" || loading) {
+        return <p>Loading...</p>;
+    }
+
     return (
         <div>
             <Header />
