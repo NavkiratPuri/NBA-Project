083a53114fab8bb2217ad6924ad98cd7cfedba8a
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-01 21:27:45 -0600

    leaders page protection

--- app/(pages)/leaders/page.js
+++ app/(pages)/leaders/page.js
@@ -3,8 +3,31 @@ import React from 'react';
 import LeadersList from '@/components/Leagueleader'; // Adjust the path if necessary
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from 'react';
 
 const Page = () => {
+
+    const { data: session, status } = useSession();
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (status === "unauthenticated") {
+          router.push("/");
+        } else if (status === "authenticated") {
+          
+            setLoading(false);
+          
+        }
+    }, [status, router]);
+
+    if (status === "loading" || loading) {
+        return <p>Loading...</p>;
+    }
+
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
