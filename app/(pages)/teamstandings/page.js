a9ec7a91209ce72c516ea87569c4e7dc1f5e4b0e
Navkirat Puri <navkirat.puri@edu.sait.ca>
2024-08-02 03:58:42 -0600

    teamstanding page loading issue fix

--- app/(pages)/teamstandings/page.js
+++ app/(pages)/teamstandings/page.js
@@ -1,30 +1,22 @@
-'use client'
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import TeamStandings from "@/components/Teamstandings";
 import Header from '@/components/header';
-//import '../app/globals.css';
 import Footer from '@/components/footer';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-
 
 const Teams = () => {
-
     const { data: session, status } = useSession();
     const router = useRouter();
-    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        if (status === "unauthenticated" ) {
-          router.push("/");
-        } 
-        if (session && !session.user.isAdmin) {
-          router.push("/");
+        if (status === "unauthenticated" || (session && !session.user.isAdmin)) {
+            router.push("/");
         }
     }, [status, session, router]);
 
-    if (status === "loading" || loading) {
+    if (status === "loading") {
         return <p>Loading...</p>;
     }
 
