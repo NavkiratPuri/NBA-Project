880c1e29a9ee6a66d562a7dad6e275bf1386a5b7
zan5786 <141464988+zan5786@users.noreply.github.com>
2024-08-08 23:47:25 -0600

    added some missing player images

--- utils/fetch60Player.js
+++ utils/fetch60Player.js
@@ -1,19 +1,19 @@
-import playerData from '@/utils/playerData';
-import calculatePlayerValue from '@/utils/calculateValue';
+import playerData from "@/utils/playerData";
+import calculatePlayerValue from "@/utils/calculateValue";
 
 // Fetch player data from the API
 const fetch60Player = async () => {
-    try {
-        const fetchdata = await playerData();
-        const filteredData = fetchdata.filter(player => {
-            const value = calculatePlayerValue(player).totalValue;
-            return value >= 80;
-        });
-        return filteredData;
-    } catch (error) {
-        console.error('Error fetching player:', error);
-        throw error;
-    }
+  try {
+    const fetchdata = await playerData();
+    const filteredData = fetchdata.filter((player) => {
+      const value = calculatePlayerValue(player).totalValue;
+      return value >= 90;
+    });
+    return filteredData;
+  } catch (error) {
+    console.error("Error fetching player:", error);
+    throw error;
+  }
 };
 
 export default fetch60Player;
